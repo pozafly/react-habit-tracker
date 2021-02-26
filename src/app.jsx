2173b70e6e852b23dfa271c712d3e3c0efdee1ff
HwangSunTae <pozafly@gmail.com>
2021-02-26 16:39:44 +0900

    완성

--- src/app.jsx
+++ src/app.jsx
@@ -16,7 +16,6 @@ class App extends Component {
     const habits = [...this.state.habits];
     const index = habits.indexOf(habit);
     habits[index].count++;
-
     this.setState({ habits });
   };
 
@@ -25,13 +24,24 @@ class App extends Component {
     const index = habits.indexOf(habit);
     const count = habits[index].count - 1;
     habits[index].count = count < 0 ? 0 : count;
-
     this.setState({ habits });
   };
 
   handleDelete = (habit) => {
     const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
 
+  handleAdd = (name) => {
+    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
     this.setState({ habits });
   };
 
@@ -46,6 +56,8 @@ class App extends Component {
           onIncrement={this.handleIncrement}
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
